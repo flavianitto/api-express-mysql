c0215316aaa64f0a0c491fe31ece60e2f5037fec
flavianitto <flavia.nnitto@gmail.com>
2022-03-22 16:49:46 -0300

    primeira rota

---
+++ index.js
@@ -0,0 +1,18 @@
+const customExpress = require('./config/customExpress');
+const conexao = require('./infraestrutura/conexao');
+const Tabelas = require('./infraestrutura/tabelas');
+
+conexao.connect((erro) => {
+    if(erro) {
+        console.log(erro);
+    }    
+    else {
+        // primeiro verificamos o erro e depois conecta no servidor
+        console.log('conectado com sucesso!');
+
+        Tabelas.init(conexao);
+        const app = customExpress();
+        
+        app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+    }
+})
